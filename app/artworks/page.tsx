ead280a42311d49ec7178025f6b36982a88bddf4
Neo Baholo <neobaholo11@gmail.com>
2023-07-16 16:18:46 +0200

    Add images lightbox

--- app/artworks/page.tsx
+++ app/artworks/page.tsx
@@ -1,20 +1,252 @@
-import React from 'react'
+'use client'
+
+import React, { useRef } from 'react'
 import styles from './page.module.css'
 import Link from 'next/link'
 import Image from 'next/image'
+import Artwork from 'public/web.images/image01.jpg'
+
+
 
 const artworks = () => {
+
+
+
   return (
     <div className={styles.container}>
       <div className={styles.heading}>
         <h1>SOUL PIECES</h1>
       </div>
       <div className={styles.content}>
-        <div className={styles.artworkgroup}>
-          <Link href="/"><Image src='/web.images/image01.jpg' alt='abundance' width={100} height={100} /></Link>
+        <div className={styles.item}>
+          <div className={styles.latestDesc}>
+            <h1 className='text-4xl font-normal mb-10'>Abundance</h1>
+            <p className={styles.para}>
+              My art is born out of the zeal for perfection both in skill, expression and devotion to create positive changes in the world. 
+              I draw inspiration from life experiences and everything that sparks a feeling of necessity.
+            </p>
+          </div>
+          <div className={styles.latestImage}>
+            <Image src={Artwork} alt='artwork' className= {styles.img}/>
+          </div>
         </div>
-      </div>
-      
+        <div className={styles.artworkgroup}>
+          <div className={styles.artworkrow} >
+            <div>
+              <Link href="/" className='flex items-center justify-center w-full'>
+                <Image src='/web.images/image01.jpg' alt='abundance' width={500} height={100} />
+              </Link>
+            </div>
+            <div>
+              <Link href="/" className='flex items-center justify-center w-full'>
+                <Image src='/web.images/image04.jpg' alt='abundance' width={500} height={100} />
+              </Link>
+            </div>
+            <div>
+              <Link href="/" className='flex items-center justify-center w-full'>
+                <Image src='/web.images/image03.jpg' alt='abundance' width={500} height={100} />
+              </Link>
+            </div>                
+          </div>
+          <div className={styles.artworkrow}>
+            <p className='text-center mt-5 text-2xl font-extralight'>Live Love  Laugh <br /><b className='text-lg'>SOLD</b></p>
+            <p className='text-center mt-5 text-2xl font-extralight'>Live Love  Laugh <br /><b className='text-lg'>SOLD</b></p>
+            <p className='text-center mt-5 text-2xl font-extralight'>Live Love  Laugh <br /><b className='text-lg'>SOLD</b></p>
+          </div>    
+        </div>
+        <div className={styles.artworkgroup}>
+          <div className={styles.artworkrow} >
+            <div>
+              <Link href="/" className='flex items-center justify-center w-full'>
+                <Image src='/web.images/image05.jpg' alt='abundance' width={500} height={100} />
+              </Link>
+            </div>
+            <div>
+              <Link href="/" className='flex items-center justify-center w-full'>
+                <Image src='/web.images/image06.jpg' alt='abundance' width={500} height={100} />
+              </Link>
+            </div>
+            <div>
+              <Link href="/" className='flex items-center justify-center w-full'>
+                <Image src='/web.images/image07.jpg' alt='abundance' width={500} height={100} />
+              </Link>
+            </div>                
+          </div>
+          <div className={styles.artworkrow}>
+            <p className='text-center mt-5 text-2xl font-extralight'>Live Love  Laugh <br /><b className='text-lg'>SOLD</b></p>
+            <p className='text-center mt-5 text-2xl font-extralight'>Live Love  Laugh <br /><b className='text-lg'>SOLD</b></p>
+            <p className='text-center mt-5 text-2xl font-extralight'>Live Love  Laugh <br /><b className='text-lg'>SOLD</b></p>
+          </div>    
+        </div>
+        <div className={styles.artworkgroup}>
+          <div className={styles.artworkrow} >
+            <div>
+              <Link href="/" className='flex items-center justify-center w-full'>
+                <Image src='/web.images/image08.jpg' alt='abundance' width={500} height={100} />
+              </Link>
+            </div>
+            <div>
+              <Link href="/" className='flex items-center justify-center w-full'>
+                <Image src='/web.images/image09.jpg' alt='abundance' width={500} height={100} />
+              </Link>
+            </div>
+            <div>
+              <Link href="/" className='flex items-center justify-center w-full'>
+                <Image src='/web.images/image10.jpg' alt='abundance' width={500} height={100} />
+              </Link>
+            </div>                
+          </div>
+          <div className={styles.artworkrow}>
+            <p className='text-center mt-5 text-2xl font-extralight'>Live Love  Laugh <br /><b className='text-lg'>SOLD</b></p>
+            <p className='text-center mt-5 text-2xl font-extralight'>Live Love  Laugh <br /><b className='text-lg'>SOLD</b></p>
+            <p className='text-center mt-5 text-2xl font-extralight'>Live Love  Laugh <br /><b className='text-lg'>SOLD</b></p>
+          </div>    
+        </div>
+        <div className={styles.artworkgroup}>
+          <div className={styles.artworkrow} >
+            <div>
+              <Link href="/" className='flex items-center justify-center w-full'>
+                <Image src='/web.images/image11.jpg' alt='abundance' width={500} height={100} />
+              </Link>
+            </div>
+            <div>
+              <Link href="/" className='flex items-center justify-center w-full'>
+                <Image src='/web.images/image12.jpg' alt='abundance' width={500} height={100} />
+              </Link>
+            </div>
+            <div>
+              <Link href="/" className='flex items-center justify-center w-full'>
+                <Image src='/web.images/image13.jpg' alt='abundance' width={500} height={100} />
+              </Link>
+            </div>                
+          </div>
+          <div className={styles.artworkrow}>
+            <p className='text-center mt-5 text-2xl font-extralight'>Live Love  Laugh <br /><b className='text-lg'>SOLD</b></p>
+            <p className='text-center mt-5 text-2xl font-extralight'>Live Love  Laugh <br /><b className='text-lg'>SOLD</b></p>
+            <p className='text-center mt-5 text-2xl font-extralight'>Live Love  Laugh <br /><b className='text-lg'>SOLD</b></p>
+          </div>    
+        </div>
+        <div className={styles.artworkgroup}>
+          <div className={styles.artworkrow} >
+            <div>
+              <Link href="/" className='flex items-center justify-center w-full'>
+                <Image src='/web.images/image14.jpg' alt='abundance' width={500} height={100} />
+              </Link>
+            </div>
+            <div>
+              <Link href="/" className='flex items-center justify-center w-full'>
+                <Image src='/web.images/image15.jpg' alt='abundance' width={500} height={100} />
+              </Link>
+            </div>
+            <div>
+              <Link href="/" className='flex items-center justify-center w-full'>
+                <Image src='/web.images/image16.jpg' alt='abundance' width={500} height={100} />
+              </Link>
+            </div>                
+          </div>
+          <div className={styles.artworkrow}>
+            <p className='text-center mt-5 text-2xl font-extralight'>Live Love  Laugh <br /><b className='text-lg'>SOLD</b></p>
+            <p className='text-center mt-5 text-2xl font-extralight'>Live Love  Laugh <br /><b className='text-lg'>SOLD</b></p>
+            <p className='text-center mt-5 text-2xl font-extralight'>Live Love  Laugh <br /><b className='text-lg'>SOLD</b></p>
+          </div>    
+        </div>
+        <div className={styles.artworkgroup}>
+          <div className={styles.artworkrow} >
+            <div>
+              <Link href="/" className='flex items-center justify-center w-full'>
+                <Image src='/web.images/image17.jpg' alt='abundance' width={500} height={100} />
+              </Link>
+            </div>
+            <div>
+              <Link href="/" className='flex items-center justify-center w-full'>
+                <Image src='/web.images/image04.jpg' alt='abundance' width={500} height={100} />
+              </Link>
+            </div>
+            <div>
+              <Link href="/" className='flex items-center justify-center w-full'>
+                <Image src='/web.images/image03.jpg' alt='abundance' width={500} height={100} />
+              </Link>
+            </div>                
+          </div>
+          <div className={styles.artworkrow}>
+            <p className='text-center mt-5 text-2xl font-extralight'>Live Love  Laugh <br /><b className='text-lg'>SOLD</b></p>
+            <p className='text-center mt-5 text-2xl font-extralight'>Live Love  Laugh <br /><b className='text-lg'>SOLD</b></p>
+            <p className='text-center mt-5 text-2xl font-extralight'>Live Love  Laugh <br /><b className='text-lg'>SOLD</b></p>
+          </div>    
+        </div>
+        <div className={styles.artworkgroup}>
+          <div className={styles.artworkrow} >
+            <div>
+              <Link href="/" className='flex items-center justify-center w-full'>
+                <Image src='/web.images/image01.jpg' alt='abundance' width={500} height={100} />
+              </Link>
+            </div>
+            <div>
+              <Link href="/" className='flex items-center justify-center w-full'>
+                <Image src='/web.images/image04.jpg' alt='abundance' width={500} height={100} />
+              </Link>
+            </div>
+            <div>
+              <Link href="/" className='flex items-center justify-center w-full'>
+                <Image src='/web.images/image03.jpg' alt='abundance' width={500} height={100} />
+              </Link>
+            </div>                
+          </div>
+          <div className={styles.artworkrow}>
+            <p className='text-center mt-5 text-2xl font-extralight'>Live Love  Laugh <br /><b className='text-lg'>SOLD</b></p>
+            <p className='text-center mt-5 text-2xl font-extralight'>Live Love  Laugh <br /><b className='text-lg'>SOLD</b></p>
+            <p className='text-center mt-5 text-2xl font-extralight'>Live Love  Laugh <br /><b className='text-lg'>SOLD</b></p>
+          </div>    
+        </div>
+        <div className={styles.artworkgroup}>
+          <div className={styles.artworkrow} >
+            <div>
+              <Link href="/" className='flex items-center justify-center w-full'>
+                <Image src='/web.images/image01.jpg' alt='abundance' width={500} height={100} />
+              </Link>
+            </div>
+            <div>
+              <Link href="/" className='flex items-center justify-center w-full'>
+                <Image src='/web.images/image04.jpg' alt='abundance' width={500} height={100} />
+              </Link>
+            </div>
+            <div>
+              <Link href="/" className='flex items-center justify-center w-full'>
+                <Image src='/web.images/image03.jpg' alt='abundance' width={500} height={100} />
+              </Link>
+            </div>                
+          </div>
+          <div className={styles.artworkrow}>
+            <p className='text-center mt-5 text-2xl font-extralight'>Live Love  Laugh <br /><b className='text-lg'>SOLD</b></p>
+            <p className='text-center mt-5 text-2xl font-extralight'>Live Love  Laugh <br /><b className='text-lg'>SOLD</b></p>
+            <p className='text-center mt-5 text-2xl font-extralight'>Live Love  Laugh <br /><b className='text-lg'>SOLD</b></p>
+          </div>    
+        </div>
+        <div className={styles.artworkgroup}>
+          <div className={styles.artworkrow} >
+            <div>
+              <Link href="/" className='flex items-center justify-center w-full'>
+                <Image src='/web.images/image01.jpg' alt='abundance' width={500} height={100} />
+              </Link>
+            </div>
+            <div>
+              <Link href="/" className='flex items-center justify-center w-full'>
+                <Image src='/web.images/image04.jpg' alt='abundance' width={500} height={100} />
+              </Link>
+            </div>
+            <div>
+              <Link href="/" className='flex items-center justify-center w-full'>
+                <Image src='/web.images/image03.jpg' alt='abundance' width={500} height={100} />
+              </Link>
+            </div>                
+          </div>
+          <div className={styles.artworkrow}>
+            <p className='text-center mt-5 text-2xl font-extralight'>Live Love  Laugh <br /><b className='text-lg'>SOLD</b></p>
+            <p className='text-center mt-5 text-2xl font-extralight'>Live Love  Laugh <br /><b className='text-lg'>SOLD</b></p>
+            <p className='text-center mt-5 text-2xl font-extralight'>Live Love  Laugh <br /><b className='text-lg'>SOLD</b></p>
+          </div>    
+        </div>
+      </div>   
     </div>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,74 @@
+'use client'
+
 
 import Image from 'next/image'
 import Link from 'next/link'
 import './globals.css'
+import Masonry from 'react-masonry-css'
+
+import type { LightGallery } from 'lightgallery/lightgallery'
+import LightGalleryComponent from 'lightgallery/react';
+
+// import styles
+import 'lightgallery/css/lightgallery.css';
+import 'lightgallery/css/lg-zoom.css';
+import 'lightgallery/css/lg-thumbnail.css'
+
+// import plugins if you need
+import lgThumbnail from 'lightgallery/plugins/thumbnail';
+import lgZoom from 'lightgallery/plugins/zoom';
+
+import image01 from "public/web.images/image01.jpg"
+import image02 from "public/web.images/image02.jpg"
+import image03 from "public/web.images/image03.jpg"
+import image04 from "public/web.images/image04.jpg"
+import { useRef } from 'react'
+
+
+const image = [image01, image02, image03, image04]
 
 export default function Home() {
+
+  const lightboxRef = useRef<LightGallery | null>(null)
+
   return (
     <main className="h-full">
       <div className='flex justify-center font-extralight'>
         <h1 className="heading">GALLERY</h1>
       </div>
       <div className='image-gallery'>
-      
+          <Masonry 
+            breakpointCols={2} 
+            className='flex gap-4' 
+            columnClassName=''
+          >
+            {image.map((image, idx)=>(
+              <Image
+                key={image.src}
+                src={image}
+                alt='placeholder'
+                className='my-4 hover:opacity-60 cursor-pointer'
+                placeholder='blur'
+                onClick={() => {
+                  lightboxRef.current?.openGallery(idx)
+                }}
+              />
+            ))}
+          </Masonry>
+          <LightGalleryComponent
+                onInit={(ref)=> {
+                  if (ref) {
+                    lightboxRef.current =ref.instance
+                  }
+                }}
+                speed={500}
+                plugins={[lgThumbnail, lgZoom]}
+                dynamic
+                dynamicEl={image.map((image) =>({
+                  src: image.src,
+                  thumb: image.src
+                }))}
+            />
       </div>
     </main>
   )
